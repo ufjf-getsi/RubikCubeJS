bc70a7825578bba777bbc808bdcea74cf40ffe25
Igor Knop <igorknop@gmail.com>
2022-05-15 12:55:13 -0300

    Adiciona um componente

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import CubeeFace from './components/CubeeFace/CubeeFace';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <CubeeFace valor="R"/>
       </header>
     </div>
   );
---
+++ src/components/CubeeFace/CubeeFace.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./CubeeFace.css";
+
+export default function CubeeFace ({ valor }) {
+  return <div className="CubeeFace">{valor}</div>;
+}
