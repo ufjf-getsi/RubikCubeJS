f3f14cfa7a687961b7b3659df324fc92c4a3cb2c
devgabmal <celsogabriel.malosto@estudante.ufjf.br>
2022-05-31 04:47:07 -0300

    Implementa visão de todas as faces do cubo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import "./App.css";
 import CubeFace from "./components/CubeFace/CubeFace";
 
 function App() {
-  let valores = ["R", "R", "R", "R", "R", "Y", "R", "R", "R"];
+  const FRONT = 0;
+  const LEFT = 1;
+  const RIGHT = 2;
+  const BACK = 3;
+  const TOP = 4;
+  const BOTTOM = 5;
+
+  const GREEN = 0;
+  const ORANGE = 1;
+  const RED = 2;
+  const BLUE = 3;
+  const WHITE = 4;
+  const YELLOW = 5;
+
+  let cube = setCubeInitialValues();
   return (
     <div className="App">
       <header className="App-header">
-
         {/*<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,10 +35,44 @@ function App() {
           Learn React
         </a>
         */}
-        <CubeFace valores={valores} />
+        <table>
+          <tbody>
+            <tr>
+              <td colSpan="2" class="toRight">
+                <CubeFace valores={cube[TOP]} />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <CubeFace valores={cube[LEFT]} />
+              </td><td>
+                <CubeFace valores={cube[FRONT]} />
+              </td>
+              <td>
+                <CubeFace valores={cube[RIGHT]} />
+              </td>
+              <td>
+                <CubeFace valores={cube[BACK]} />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2" class="toRight">
+                <CubeFace valores={cube[BOTTOM]} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </header>
     </div>
   );
 }
 
 export default App;
+
+function setCubeInitialValues() {
+  let cube = [];
+  for (let i = 0; i < 6; i++) {
+    cube.push([i, i, i, i, i, i, i, i, i]);
+  }
+  return cube;
+}
--- src/components/CubeFace/CubeFace.js
+++ src/components/CubeFace/CubeFace.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./CubeFace.css";
 import SingleSquare from "../SingleSquare/SingleSquare.js";
 
 export default function CubeFace({ valores }) {
@@ -9,16 +10,18 @@ export default function CubeFace({ valores }) {
     let elemLin = [];
 
     for (let j = i * 3; j < (i * 3) + 3; j++) {
-      elemLin.push(<td><SingleSquare valor={valores[j]} key={j} /></td>);
+      elemLin.push(<td key={j}><SingleSquare valor={valores[j]} key={j} /></td>);
     }
 
-    linhas.push(<tr>{elemLin}</tr>);
+    linhas.push(<tr key={i}>{elemLin}</tr>);
   }
 
   return (
     <div className="CubeFace">
       <table>
-        {linhas}
+        <tbody>
+          {linhas}
+        </tbody>
       </table>
     </div>
   );
