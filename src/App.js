e17f8d3a84bf95a1abca6476c56c68c0f766ec47
devgabmal <celsogabriel.malosto@estudante.ufjf.br>
2022-05-31 07:23:02 -0300

    Inicia implementação das operações de movimento

--- src/App.js
+++ src/App.js
@@ -4,19 +4,57 @@ import "./App.css";
 import CubeFace from "./components/CubeFace/CubeFace";
 import MoveButton from './components/MoveButton/MoveButton';
 
-function App() {
-  const FRONT = 0;
-  const LEFT = 1;
-  const RIGHT = 2;
-  const BACK = 3;
-  const TOP = 4;
-  const BOTTOM = 5;
+// Constant
+const DI = 3; // cube dimensions
+const FRONT = 0;
+const LEFT = 1;
+const RIGHT = 2;
+const BACK = 3;
+const TOP = 4;
+const BOTTOM = 5;
 
-  let cube = setCubeInitialValues();
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cube: setCubeInitialValues(),
+    };
+  }
+
+  handleClick(val) {
+    let cube = this.state.cube;
+    switch (val) {
+      case "l":
+        slideCol(0, cube[FRONT], cube[TOP], cube[BACK], cube[BOTTOM]);
+        rotateFaceCounterClockwise(cube[LEFT]);
+        console.log(cube[FRONT]);
+        this.state = {
+          cube: cube,
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  renderCube(val) {
+    return (
+      <CubeFace values={this.state.cube[val]} />
+    );
+  }
+
+  renderButton(val) {
+    return (
+      <td><MoveButton value={val} onClick={() => this.handleClick(val)} /></td>
+    );
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          {/*<img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -29,64 +67,199 @@ function App() {
           Learn React
         </a>
         */}
-        <table>
-          <tbody>
-            <tr>
-              <td colSpan="2" className="toRight">
-                <CubeFace values={cube[TOP]} />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <CubeFace values={cube[LEFT]} />
-              </td><td>
-                <CubeFace values={cube[FRONT]} />
-              </td>
-              <td>
-                <CubeFace values={cube[RIGHT]} />
-              </td>
-              <td>
-                <CubeFace values={cube[BACK]} />
-              </td>
-            </tr>
-            <tr>
-              <td colSpan="2" className="toRight">
-                <CubeFace values={cube[BOTTOM]} />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <table className="buttonTable">
-          <tbody>
-            <tr>
-              <td><MoveButton value="L" /></td>
-              <td><MoveButton value="R" /></td>
-              <td><MoveButton value="U" /></td>
-              <td><MoveButton value="D" /></td>
-              <td><MoveButton value="F" /></td>
-              <td><MoveButton value="B" /></td>
-            </tr>
-            <tr>
-              <td><MoveButton value="l" /></td>
-              <td><MoveButton value="r" /></td>
-              <td><MoveButton value="u" /></td>
-              <td><MoveButton value="d" /></td>
-              <td><MoveButton value="f" /></td>
-              <td><MoveButton value="b" /></td>
-            </tr>
-          </tbody>
-        </table>
-      </header>
-    </div>
-  );
+          <table>
+            <tbody>
+              <tr>
+                <td colSpan="2" className="toRight">
+                  {this.renderCube(TOP)}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  {this.renderCube(LEFT)}
+                </td><td>
+                  {this.renderCube(FRONT)}
+                </td>
+                <td>
+                  {this.renderCube(RIGHT)}
+                </td>
+                <td>
+                  {this.renderCube(BACK)}
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="2" className="toRight">
+                  {this.renderCube(BOTTOM)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <table className="buttonTable">
+            <tbody>
+              <tr>
+                {this.renderButton("L")}
+                {this.renderButton("R")}
+                {this.renderButton("U")}
+                {this.renderButton("D")}
+                {this.renderButton("F")}
+                {this.renderButton("B")}
+              </tr>
+              <tr>
+                {this.renderButton("l")}
+                {this.renderButton("r")}
+                {this.renderButton("u")}
+                {this.renderButton("d")}
+                {this.renderButton("f")}
+                {this.renderButton("b")}
+              </tr>
+            </tbody>
+          </table>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
 
 function setCubeInitialValues() {
-  let cube = [];
+  let cube = new Array();
   for (let i = 0; i < 6; i++) {
-    cube.push([i, i, i, i, i, i, i, i, i]);
+    cube.push([[i, i, i], [i, i, i], [i, i, i]]);
   }
   return cube;
+}
+
+function slideRow(row, face1, face2, face3, face4) {
+  let newRow2 = [];
+  let newRow3 = [];
+  for (let c = 0; c < DI; c++) {
+    newRow2[c] = face1[row][c];
+  }
+  for (let c = 0; c < DI; c++) {
+    face1[row][c] = face4[row][c];
+    face4[row][c] = face3[row][DI - 1 - c];
+    newRow3[c] = face2[row][DI - 1 - c];
+  }
+  for (let c = 0; c < DI; c++) {
+    face2[row][c] = newRow2[c];
+    face3[row][c] = newRow3[c];
+  }
+}
+
+function slideRowInverted(row, face1, face2, face3, face4) {
+  let newRow4 = [];
+  let newRow3 = [];
+  for (let c = 0; c < DI; c++) {
+    newRow4[c] = face1[row][c];
+  }
+  for (let c = 0; c < DI; c++) {
+    face1[row][c] = face2[row][c];
+    face2[row][c] = face3[row][DI - 1 - c];
+    newRow3[c] = face4[row][DI - 1 - c];
+  }
+  for (let c = 0; c < DI; c++) {
+    face4[row][c] = newRow4[c];
+    face3[row][c] = newRow3[c];
+  }
+}
+
+function slideCol(col, face1, face2, face3, face4) {
+  let newCol2 = [];
+  let newCol3 = [];
+  for (let r = 0; r < DI; r++) {
+    newCol2[r] = face1[r][col];
+  }
+  for (let r = 0; r < DI; r++) {
+    face1[r][col] = face4[r][col];
+    face4[r][col] = face3[DI - 1 - r][col];
+    newCol3[r] = face2[DI - 1 - r][col];
+  }
+  for (let r = 0; r < DI; r++) {
+    face2[r][col] = newCol2[r];
+    face3[r][col] = newCol3[r];
+  }
+}
+
+function slideColInverted(col, face1, face2, face3, face4) {
+  let newCol4 = [];
+  let newCol3 = [];
+  for (let r = 0; r < DI; r++) {
+    newCol4[r] = face1[r][col];
+  }
+  for (let r = 0; r < DI; r++) {
+    face1[r][col] = face2[r][col];
+    face2[r][col] = face3[DI - 1 - r][col];
+    newCol3[r] = face4[DI - 1 - r][col];
+  }
+  for (let r = 0; r < DI; r++) {
+    face4[r][col] = newCol4[r];
+    face3[r][col] = newCol3[r];
+  }
+}
+
+function slideSideCol(col, face1, face2, face3, face4) {
+  let newCol1 = [];
+  let newCol2 = [];
+  let newCol3 = [];
+  let newCol4 = [];
+  for (let r = 0; r < DI; r++) {
+    newCol1[r] = face4[col][DI - 1 - r];
+    newCol2[r] = face1[r][col];
+    newCol3[r] = face2[DI - 1 - col][DI - 1 - r];
+    newCol4[r] = face3[r][DI - 1 - col];
+  }
+  for (let r = 0; r < DI; r++) {
+    face1[r][col] = newCol1[r];
+    face2[DI - 1 - col][r] = newCol2[r];
+    face3[r][DI - 1 - col] = newCol3[r];
+    face4[col][r] = newCol4[r];
+  }
+}
+
+function slideSideColInverted(col, face1, face2, face3, face4) {
+  let newCol1 = [];
+  let newCol2 = [];
+  let newCol3 = [];
+  let newCol4 = [];
+  for (let r = 0; r < DI; r++) {
+    newCol1[r] = face2[DI - 1 - col][r];
+    newCol2[r] = face3[DI - 1 - r][DI - 1 - col];
+    newCol3[r] = face4[col][r];
+    newCol4[r] = face1[DI - 1 - r][col];
+  }
+  for (let r = 0; r < DI; r++) {
+    face1[r][col] = newCol1[r];
+    face2[DI - 1 - col][r] = newCol2[r];
+    face3[r][DI - 1 - col] = newCol3[r];
+    face4[col][r] = newCol4[r];
+  }
+}
+
+function rotateFaceClockwise(face) {
+  let newFace = [[], [], []];
+  for (let r = 0; r < DI; r++) {
+    for (let c = 0; c < DI; c++) {
+      newFace[r][c] = face[DI - 1 - c][r];
+    }
+  }
+  for (let r = 0; r < DI; r++) {
+    for (let c = 0; c < DI; c++) {
+      face[r][c] = newFace[r][c];
+    }
+  }
+}
+
+function rotateFaceCounterClockwise(face) {
+  let newFace = [[], [], []];
+  for (let r = 0; r < DI; r++) {
+    for (let c = 0; c < DI; c++) {
+      newFace[r][c] = face[c][DI - 1 - r];
+    }
+  }
+  for (let r = 0; r < DI; r++) {
+    for (let c = 0; c < DI; c++) {
+      face[r][c] = newFace[r][c];
+    }
+  }
 }
--- src/components/CubeFace/CubeFace.js
+++ src/components/CubeFace/CubeFace.js
@@ -3,14 +3,13 @@ import "./CubeFace.css";
 import SingleSquare from "../SingleSquare/SingleSquare.js";
 
 export default function CubeFace({ values }) {
-
   let rows = [];
 
   for (let i = 0; i < 3; i++) {
     let elemRow = [];
 
-    for (let j = i * 3; j < (i * 3) + 3; j++) {
-      elemRow.push(<td key={j}><SingleSquare value={values[j]} key={j} /></td>);
+    for (let j = 0; j < 3; j++) {
+      elemRow.push(<td key={j}><SingleSquare value={values[i][j]} key={j} /></td>);
     }
 
     rows.push(<tr key={i}>{elemRow}</tr>);
