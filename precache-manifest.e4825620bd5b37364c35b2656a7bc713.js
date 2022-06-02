1717a9b58fb28f60d5b6be0e308b98e36baed44a
Igor Knop <igorknop@gmail.com>
2022-06-02 15:40:54 -0300

    Updates

---
+++ precache-manifest.e4825620bd5b37364c35b2656a7bc713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4753f59666bb41ad6668",
+    "url": "/RubikCubeJS/static/css/main.dea286ea.chunk.css"
+  },
+  {
+    "revision": "4753f59666bb41ad6668",
+    "url": "/RubikCubeJS/static/js/main.4753f596.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/RubikCubeJS/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "5ad78d50cf31a7c476ea",
+    "url": "/RubikCubeJS/static/js/2.5ad78d50.chunk.js"
+  },
+  {
+    "revision": "7dd370953766c471cf72",
+    "url": "/RubikCubeJS/static/js/runtime~main.7dd37095.js"
+  },
+  {
+    "revision": "d5b0d59c14d86bb5f99154d36ccea59d",
+    "url": "/RubikCubeJS/index.html"
+  }
+];
