165ccf6ec9929432e4c39ced1c4bf3394561f05e
devgabmal <celsogabriel.malosto@estudante.ufjf.br>
2022-06-03 00:49:44 -0300

    Exibe face traseira na ordem reversa pelo CSS

--- src/components/CubeFace/CubeFace.js
+++ src/components/CubeFace/CubeFace.js
@@ -6,10 +6,7 @@ export default function CubeFace({ squares, face }) {
   const elementSquares = [];
   for (let l = 0; l < squares.length; l++) {
     for (let c = 0; c < squares[0].length; c++) {
-      if (face === 3)// Render back face
-        elementSquares.push(<SingleSquare value={squares[l][2 - c]} key={face * 9 + l * 3 + c} />);
-      else
-        elementSquares.push(<SingleSquare value={squares[l][c]} key={face * 9 + l * 3 + c} />);
+      elementSquares.push(<SingleSquare value={squares[l][c]} key={face * 9 + l * 3 + c} />);
     }
   }
   const className = `cube-face face-${face}`;
