53c45d10fd159d0c2c4ab4da3cde802621c7335e
devgabmal <celsogabriel.malosto@estudante.ufjf.br>
2022-06-03 00:03:24 -0300

    Corrige exibição da face traseira

--- src/components/CubeFace/CubeFace.js
+++ src/components/CubeFace/CubeFace.js
@@ -6,7 +6,10 @@ export default function CubeFace({ squares, face }) {
   const elementSquares = [];
   for (let l = 0; l < squares.length; l++) {
     for (let c = 0; c < squares[0].length; c++) {
-      elementSquares.push(<SingleSquare value={squares[l][c]} key={face * 9 + l * 3 + c} />);
+      if (face === 3)// Render back face
+        elementSquares.push(<SingleSquare value={squares[l][2 - c]} key={face * 9 + l * 3 + c} />);
+      else
+        elementSquares.push(<SingleSquare value={squares[l][c]} key={face * 9 + l * 3 + c} />);
     }
   }
   const className = `cube-face face-${face}`;
