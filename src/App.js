4fbf1b6f8d7de08bff720d4bdac59e78a83a1160
devgabmal <celsogabriel.malosto@estudante.ufjf.br>
2022-06-02 23:19:10 -0300

    Adiciona chaves aos elementes em repetição

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default function App() {
       character += "'";
     }
     elementButtons.push(
-      <button className="move-button" onClick={function click() { doCubeMove(character); }}>{character}</button>
+      <button className="move-button" key={b} onClick={function click() { doCubeMove(character); }}>{character}</button>
     );
   }
 
