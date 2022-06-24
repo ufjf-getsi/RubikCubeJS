6c0ed652faa741995ccbf755f4409b819dd1ab69
LucasPaivaSantos <lucaspaiva@live.com>
2022-06-24 12:54:38 -0300

    associa "valor" a cada face de SingleCube

--- src/components/SingleCube/SingleCube.js
+++ src/components/SingleCube/SingleCube.js
@@ -1,16 +1,17 @@
 import React, { useRef } from "react";
 
-export default function SingleCube({ position, faces }) {
+export default function SingleCube({ position, value }) {
   const mesh = useRef();
+  const className = `singeFaceColor${value}`;
   return (
-    <mesh position={position} ref={mesh}>
+    <mesh position={position} className={className} ref={mesh}>
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshLambertMaterial attach={`material-0`} color="red" />
-      <meshLambertMaterial attach={`material-1`} color="darkOrange" />
-      <meshLambertMaterial attach={`material-2`} color="white" />
-      <meshLambertMaterial attach={`material-3`} color="yellow" />
-      <meshLambertMaterial attach={`material-4`} color="green" />
-      <meshLambertMaterial attach={`material-5`} color="blue" />
+      <meshLambertMaterial attach={`singeFaceColor${value}`} />
+      <meshLambertMaterial attach={`singeFaceColor${value}`} />
+      <meshLambertMaterial attach={`singeFaceColor${value}`} />
+      <meshLambertMaterial attach={`singeFaceColor${value}`} />
+      <meshLambertMaterial attach={`singeFaceColor${value}`} />
+      <meshLambertMaterial attach={`singeFaceColor${value}`} />
     </mesh>
   );
 }
