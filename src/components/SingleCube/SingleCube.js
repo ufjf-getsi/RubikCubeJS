67b78c00b723dec02dd4acba499bba255ab28e2d
LucasPaivaSantos <lucaspaiva@live.com>
2022-06-25 11:52:04 -0300

    refatora singleCube.js

--- src/components/SingleCube/SingleCube.js
+++ src/components/SingleCube/SingleCube.js
@@ -2,16 +2,16 @@ import React, { useRef } from "react";
 
 export default function SingleCube({ position, value }) {
   const mesh = useRef();
-  const className = `singeFaceColor${value}`;
+
   return (
-    <mesh position={position} className={className} ref={mesh}>
+    <mesh position={position} ref={mesh}>
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshLambertMaterial attach={`singeFaceColor${value}`} />
-      <meshLambertMaterial attach={`singeFaceColor${value}`} />
-      <meshLambertMaterial attach={`singeFaceColor${value}`} />
-      <meshLambertMaterial attach={`singeFaceColor${value}`} />
-      <meshLambertMaterial attach={`singeFaceColor${value}`} />
-      <meshLambertMaterial attach={`singeFaceColor${value}`} />
+      <meshLambertMaterial attach={`material-${value}`} color="red" />
+      <meshLambertMaterial attach={`material-${value}`} color="darkOrange" />
+      <meshLambertMaterial attach={`material-${value}`} color="white" />
+      <meshLambertMaterial attach={`material-${value}`} color="yellow" />
+      <meshLambertMaterial attach={`material-${value}`} color="green" />
+      <meshLambertMaterial attach={`material-${value}`} color="blue" />
     </mesh>
   );
 }
