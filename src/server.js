e65622f29e97133d428c1267cdf74423e5fff545
Igor Knop <igorknop@gmail.com>
2022-08-24 15:24:40 -0300

    fix homepage

--- src/server.js
+++ src/server.js
@@ -1,41 +1,48 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const express = require("express");
+const path = require("path");
+const app = express();
+
 const PORT = process.env.PORT || 3000;
 const PORT_SOCKET = process.env.PORT_SOCKET || 3001;
 
+app.use(express.static(path.join(__dirname, "../build")));
 
-const { createServer } = require('http');
-const { Server } = require('socket.io');
-const express = require("express");
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+app.listen(PORT);
+console.log("Listening http on port " + PORT + "...");
 
-const server = express()
-  .use(express.static(`build`))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const httpServer = createServer()
+const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
-    origin: '*'
-  }
-})
+    origin: "*",
+  },
+});
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`${socket.id} conectado.`);
 
   socket.on("join_room", (data) => {
     socket.join(data);
   });
 
-  socket.on('send_message', (data)=>{
+  socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
   });
 
-  socket.on('send_cube', (data)=>{
+  socket.on("send_cube", (data) => {
     socket.to(data.room).emit("move_cube", data);
   });
 
-  socket.on('initial_cube', (data)=>{
+  socket.on("initial_cube", (data) => {
     socket.to(data.room).emit("set_cube");
   });
 });
 
-httpServer.listen(PORT_SOCKET)
-console.log('Listening on port ' + PORT_SOCKET + '...')
+httpServer.listen(PORT_SOCKET);
+console.log("Listening socket on port " + PORT_SOCKET + "...");
